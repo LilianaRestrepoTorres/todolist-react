4be74a938267d14e68bcb657c40075ed0506719a
lilianarestrepotorres <lilianarestrepot@gmail.com>
2022-03-25 01:09:36 -0400

    Initial Commit

---
+++ src/TodoCounter.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './TodoCounter.css';
+
+function TodoCounter(){
+    return(
+        <h2 className="TodoCounter">You've finished 2 of 3 TODOs</h2>
+    );
+}
+
+export { TodoCounter };
